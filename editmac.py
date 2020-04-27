2b9cff0c64c0336e109746fa7d004cc7ddbe8bad
iamirror <64370142+iamirror@users.noreply.github.com>
2020-04-27 15:11:10 +0530

    Add files via upload
    new version, run via variable intiliziation

---
+++ editmac.py
@@ -0,0 +1,29 @@
+import subprocess
+import pyfiglet
+import optparse
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="NEW MAC address")
+    (value, arguments) =  parser.parse_args()
+    if not value.interface:
+        parser.error("[-] please specify an interface , use --help for more info")
+    elif not value.new_mac:
+        parser.error("[-] please specify an new mac , use --help for more info")
+    return value
+
+
+def mac_changer(interface, new_mac):
+    print("[+] Changing mac address for" + interface + "to" + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+ascii_banner = pyfiglet.figlet_format("Coded  by  Mirror!!")
+print(ascii_banner)
+
+value = get_arguments()
+mac_changer(value.interface, value.new_mac)
